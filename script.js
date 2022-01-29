4e212dcad8b168c4eeeb5d65c9d16202f9a923a6
Juan Cruz <juancruzpalau@gmail.com>
2022-01-29 19:14:47 -0300

    script.js

--- script.js
+++ script.js
@@ -1,54 +1,86 @@
-var computerScore = 0;
-var playerScore = 0;
-var round = 0;
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
 
-function computerPlay() {
-    let n = Math.floor(Math.random() * 3) + 1;
-    if (n == 1) {
-        n = 'rock'
-    } else if (n == 2) {
-        n = 'paper'
-    } else {
-        n = 'scissors'
-    }
-    return n
+let playerScore = 0;
+let computerScore = 0;
+
+// Event Listeners
+choices.forEach(choice=>choice.addEventListener('click', play));
+restart.addEventListener('click', restartGame());
+
+// Play Game
+function play(e) {
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+    showWinner = (winner, computerChoice);
 }
 
-function roundPlay (playerSelection, computerSelection) {
-    computerPlay();
-    computerSelection = computerPlay();
-    playerSelection = prompt ('Rock, Paper or Scissors?');
-    playerSelection = playerSelection.toLowerCase();
-    round += 1;
-    
-    if ((playerSelection == 'rock') && (computerSelection == 'paper') ||
-        (playerSelection == 'paper') && (computerSelection == 'scissors') ||
-        (playerSelection == 'scissors') && (computerSelection == 'rock')) {
-            computerScore += 1;
-            return "You Lose!"
-    } else if ((playerSelection == 'rock') && (computerSelection == 'scissors') ||
-    (playerSelection == 'paper') && (computerSelection == 'rock') ||
-    (playerSelection == 'scissors') && (computerSelection == 'paper')) {
-            playerScore += 1;
-            return "You Win!"
+//Computer Choice
+function getComputerChoice() {
+    let random = Math.floor(Math.random()*3 + 1);
+    if (random === 1) {
+        return 'rock';
+    } else if (random === 2) {
+            return 'paper';
     } else {
-            return "It's a tie!"
-    }
+            return 'scissors';
+        }
 }
 
-function game() {
-    roundPlay();
-    roundPlay();
-    roundPlay();
-    roundPlay();
-    roundPlay();
-
-    if (playerScore > 2) {
-        console.log ("Congratulations! You won!")
-    } else {
-        console.log ("Game Over.")
+//Winner
+function getWinner (playerChoice, computerChoice) {
+    if (playerChoice === 'rock' && computerChoice === 'scissors' || 
+        playerChoice === 'scissors' && computerChoice === 'paper' ||
+        playerChoice === 'paper' && computerChoice === 'rock') {
+        return ('You win!' + playerChoice + 'beats' + computerChoice + '.');
+    }
+    else if (playerChoice === 'paper' && computerChoice === 'scissors' || 
+        playerChoice === 'rock' && computerChoice === 'paper' ||
+        playerChoice === 'scissors' && computerChoice === 'rock') { 
+        return ('You lose!' + computerChoice + 'beats' + playerChoice + '.');
+    } else if (playerChoice === computerChoice) {
+        return ('It\'s a tie!')
     }
 }
 
+// Show winner
+function showWinner (winner, computerChoice) {
+    if (winner === player) {
+        playerScore++;
+        result.innerHTML = `
+            <h1 class="text-win"> You win! </h1>
+            <i class= "fas fa-hand-${computerChoice} fa-10px"></i>
+            <p>Computer chose ${computerChoice}.</p> `
+            
+    } else if (winner === computer) {
+        computerScore++;
+        result.innerHTML = `
+            <h1 class="text-lose"> You lose! </h1>;
+            <i class= "fas fa-hand-${computerChoice} fa-10px"></i>
+            <p>Computer chose ${computerChoice}.</p> `
+    } else {
+        result.innerHTML = `
+            <h1 class="text-lose"> It's a draw! </h1>;
+            <i class= "fa-hand-${computerChoice} fa-10px"></i>
+            <p>Computer chose ${computerChoice}.</p> `
+    }
 
 
+// Show score
+score.innerHTML = ` <p> Player: ${scoreboard.player} </p>
+                    <p> Computer: ${scoreboard.computer} </p> ` 
+
+modal.style.display = 'block';
+
+}
+
+function restartGame () {
+    playerScore = 0;
+    computerScore = 0;
+    score.innerHTML = `
+                    <p>Player: 0</p>
+                    <p>Computer: 0</p> `
+}
